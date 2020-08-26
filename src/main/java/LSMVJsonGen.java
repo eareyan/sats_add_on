782f209cdc47691322f8893e50cf131a6618745e
eareyan <enrique3@gmail.com>
2020-08-26 12:57:44 -0400

    Added GSVM

--- src/main/java/LSMVJsonGen.java
+++ src/main/java/LSMVJsonGen.java
@@ -1,60 +1,65 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
 import org.spectrumauctions.sats.core.model.Bundle;
+import org.spectrumauctions.sats.core.model.gsvm.GSVMBidder;
+import org.spectrumauctions.sats.core.model.gsvm.GSVMLicense;
+import org.spectrumauctions.sats.core.model.gsvm.GSVMWorld;
+import org.spectrumauctions.sats.core.model.gsvm.GlobalSynergyValueModel;
 import org.spectrumauctions.sats.core.model.lsvm.LSVMBidder;
 import org.spectrumauctions.sats.core.model.lsvm.LSVMLicense;
 import org.spectrumauctions.sats.core.model.lsvm.LSVMWorld;
 import org.spectrumauctions.sats.core.model.lsvm.LocalSynergyValueModel;
 
-import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.*;
 
-class LSVMDraw {
+class ValueModelDraw {
     /**
-     * Represents a draw of the LSVM model. A draw consists of a list of bidder, each with all possible values for
+     * Represents a draw of either the LSVM or GSVM model.
+     * A draw consists of a list of bidders, each with all possible values for
      * all possible bundles (for which they have non-zero value).
      */
-    public List<LSVMBidderValues> bidder_values;
+    public List<BidderValues> bidder_values;
 
-    public LSVMDraw(List<LSVMBidderValues> bidder_values) {
+    public ValueModelDraw(List<BidderValues> bidder_values) {
         this.bidder_values = bidder_values;
     }
 }
 
-class BundleValueTuple {
+class Tuple<L, R> {
     /**
-     * A (Bundle, Value) tuple.
+     * A simple tuple class. We annotate with how we want to print values for the json file.
      */
-    public List<Integer> bundle;
-    public BigDecimal value;
+    @SerializedName("bundle")
+    public final L l;
+    @SerializedName("value")
+    public final R r;
 
-    public BundleValueTuple(List<Integer> bundle, BigDecimal value) {
-        this.bundle = bundle;
-        Collections.sort(this.bundle);
-        this.value = value;
+    public Tuple(L l, R r) {
+        this.l = l;
+        this.r = r;
     }
 }
 
-class LSVMBidderValues {
+class BidderValues {
     /**
      * Represents the values for bundles a bidder has.
      */
     public int id;
     public List<Integer> preferred_licences;
-    public List<BundleValueTuple> values;
+    public List<Tuple<List<Integer>, BigDecimal>> values;
 
-    public LSVMBidderValues(int id, List<Integer> preferred_licences, List<BundleValueTuple> values) {
+    public BidderValues(int id, List<Integer> preferred_licences, List<Tuple<List<Integer>, BigDecimal>> values) {
         this.id = id;
         this.preferred_licences = preferred_licences;
         Collections.sort(this.preferred_licences);
         this.values = values;
-
     }
 }
 
-class CompareBundleValueTuple implements Comparator<BundleValueTuple> {
+class CompareBundleValueTuple implements Comparator<Tuple<List<Integer>, BigDecimal>> {
     /**
      * Used to order the way in which bundle and values are reported.
      *
@@ -63,16 +68,78 @@ class CompareBundleValueTuple implements Comparator<BundleValueTuple> {
      * @return 1, -1, or 0
      */
     @Override
-    public int compare(BundleValueTuple leftBundleValueTuple, BundleValueTuple rightBundleValueTuple) {
-        if (leftBundleValueTuple.bundle.size() > rightBundleValueTuple.bundle.size()) {
+    public int compare(Tuple<List<Integer>, BigDecimal> leftBundleValueTuple, Tuple<List<Integer>, BigDecimal> rightBundleValueTuple) {
+        if (leftBundleValueTuple.l.size() > rightBundleValueTuple.l.size()) {
             return 1;
-        } else if (leftBundleValueTuple.bundle.size() < rightBundleValueTuple.bundle.size()) {
+        } else if (leftBundleValueTuple.l.size() < rightBundleValueTuple.l.size()) {
             return -1;
         } else {
-            return leftBundleValueTuple.value.compareTo(rightBundleValueTuple.value);
+            return leftBundleValueTuple.r.compareTo(rightBundleValueTuple.r);
         }
     }
 }
+
+class ValueBidder {
+    /**
+     * A triage class to handle both LSVM and GSVM bidders.
+     */
+
+    public final Tuple<LSVMBidder, GSVMBidder> bidderTuple;
+    public final Tuple<List<LSVMLicense>, List<GSVMLicense>> licensesTuple;
+    public final String type;
+
+    public ValueBidder(Tuple<LSVMBidder, GSVMBidder> bidderTuple,
+                       Tuple<List<LSVMLicense>, List<GSVMLicense>> licensesTuple,
+                       String type) throws Exception {
+        this.bidderTuple = bidderTuple;
+        this.licensesTuple = licensesTuple;
+        this.type = type;
+        if (!type.equals("LSVM") && !type.equals("GSVM")) {
+            throw new Exception("Either LSVM or GSVM");
+        }
+    }
+
+    public Map<Long, BigDecimal> getBaseValues() {
+        if (this.type.equals("LSVM")) {
+            return this.bidderTuple.l.getBaseValues();
+        } else {
+            return this.bidderTuple.r.getBaseValues();
+        }
+    }
+
+    public BigDecimal calculateValue(List<Integer> bundle) {
+        if (this.type.equals("LSVM")) {
+            return this.bidderTuple.l.calculateValue(this.get_value_bundle(bundle).l);
+        } else {
+            return this.bidderTuple.r.calculateValue(this.get_value_bundle(bundle).r);
+        }
+    }
+
+    public long getId() {
+        if (this.type.equals("LSVM")) {
+            return this.bidderTuple.l.getId();
+        } else {
+            return this.bidderTuple.r.getId();
+        }
+    }
+
+    public Tuple<Bundle<LSVMLicense>, Bundle<GSVMLicense>> get_value_bundle(List<Integer> bundle) {
+        if (this.type.equals("LSVM")) {
+            Bundle<LSVMLicense> lsvm_bundle = new Bundle<>();
+            for (Integer lic : bundle) {
+                lsvm_bundle.add(this.licensesTuple.l.get(lic));
+            }
+            return new Tuple<>(lsvm_bundle, null);
+        } else {
+            Bundle<GSVMLicense> gsvm_bundle = new Bundle<>();
+            for (Integer lic : bundle) {
+                gsvm_bundle.add(this.licensesTuple.r.get(lic));
+            }
+            return new Tuple<>(null, gsvm_bundle);
+        }
+    }
+}
+
 
 public class LSMVJsonGen {
 
@@ -108,28 +175,12 @@ public class LSMVJsonGen {
         return LSMVJsonGen.enumerate_helper(new ArrayList<>(), listOfNumbers);
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        String jsonOutputFileLocation;
-        if (args.length == 0) {
-            jsonOutputFileLocation = "default.json";
-        } else {
-            jsonOutputFileLocation = args[0];
-        }
-
+    public static ValueModelDraw create_lsvm_draw(List<ValueBidder> bidders) {
         // Data structures to collect results
-        List<LSVMBidderValues> lsvmBidderValues = new ArrayList<>();
-
-        // Create LSVM model.
-        LocalSynergyValueModel lsvm = new LocalSynergyValueModel();
-        LSVMWorld world = lsvm.createWorld();
-        List<LSVMLicense> licenses = world.getLicenses().asList();
-        List<LSVMBidder> bidders = lsvm.createPopulation(world);
+        List<BidderValues> lsvmBidderValues = new ArrayList<>();
 
         // For each bidder, find the bundles the bidder is interested in.
-        for (LSVMBidder bidder : bidders) {
-
-            // Ignoring the National bidder, for now.
-            if (bidder.getId() == 0) continue;
+        for (ValueBidder bidder : bidders) {
 
             // Licenses are given as Long. Collect them in a list of integers so that we can enumerate over them.
             List<Integer> bidders_licenses = new ArrayList<>();
@@ -138,25 +189,65 @@ public class LSMVJsonGen {
             }
 
             // Enumerate all possible bundles.
-            List<BundleValueTuple> bidder_bundles_values_list = new ArrayList<>();
+            List<Tuple<List<Integer>, BigDecimal>> bidder_bundles_values_list = new ArrayList<>();
             Set<List<Integer>> all_bundles = enumerate(bidders_licenses);
             for (List<Integer> bundle : all_bundles) {
-                Bundle<LSVMLicense> lsvm_bundle = new Bundle<>();
-                for (Integer lic : bundle) {
-                    lsvm_bundle.add(licenses.get(lic));
-                }
-                //System.out.println(lsvm_bundle);
-                bidder_bundles_values_list.add(new BundleValueTuple(bundle, bidder.calculateValue(lsvm_bundle).setScale(8, BigDecimal.ROUND_HALF_UP)));
+                bidder_bundles_values_list.add(new Tuple<>(bundle, bidder.calculateValue(bundle).setScale(8, BigDecimal.ROUND_HALF_UP)));
             }
             bidder_bundles_values_list.sort(new CompareBundleValueTuple());
-            LSVMBidderValues bidder_value = new LSVMBidderValues((int) bidder.getId(), bidders_licenses, bidder_bundles_values_list);
+            BidderValues bidder_value = new BidderValues((int) bidder.getId(), bidders_licenses, bidder_bundles_values_list);
             lsvmBidderValues.add(bidder_value);
         }
-        LSVMDraw lsvm_draw = new LSVMDraw(lsvmBidderValues);
+        return new ValueModelDraw(lsvmBidderValues);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("log4j.configurationFile", "log4j2.xml");
+
+        // Check we have the right number of parameters.
+        if (args.length != 1 && args.length != 2) {
+            throw new Exception("Either 1 or 2 arguments. ");
+        }
+
+        // Get the model type.
+        String model_type = args[0];
+
+        if (!model_type.equals("LSVM") && !model_type.equals("GSVM")) {
+            throw new Exception("Model type must be either LSVM of GSVM");
+        }
+
+        // Get the output file location.
+        String jsonOutputFileLocation;
+        if (args.length == 1) {
+            jsonOutputFileLocation = "default.json";
+        } else {
+            jsonOutputFileLocation = args[1];
+        }
+
+        List<ValueBidder> valueBidders = new ArrayList<>();
+        if (model_type.equals("LSVM")) {
+            // Create LSVM model.
+            LocalSynergyValueModel lsvm = new LocalSynergyValueModel();
+            LSVMWorld lsvmWorld = lsvm.createWorld();
+            List<LSVMLicense> lsvmLicenses = lsvmWorld.getLicenses().asList();
+            List<LSVMBidder> lsvmBidders = lsvm.createPopulation(lsvmWorld);
+            for (LSVMBidder lsvmBidder : lsvmBidders) {
+                valueBidders.add(new ValueBidder(new Tuple<>(lsvmBidder, null), new Tuple<>(lsvmLicenses, null), "LSVM"));
+            }
+        } else {
+            // Create GSVM model.
+            GlobalSynergyValueModel gsvm = new GlobalSynergyValueModel();
+            GSVMWorld gsvmWorld = gsvm.createWorld();
+            List<GSVMLicense> gsvmLicenses = gsvmWorld.getLicenses().asList();
+            List<GSVMBidder> gsvmBidders = gsvm.createPopulation(gsvmWorld);
+            for (GSVMBidder gsvmBidder : gsvmBidders) {
+                valueBidders.add(new ValueBidder(new Tuple<>(null, gsvmBidder), new Tuple<>(null, gsvmLicenses), "GSVM"));
+            }
+        }
 
         // Create and save JSON file.
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json_lsvm_draw = gson.toJson(lsvm_draw);
+        String json_lsvm_draw = gson.toJson(create_lsvm_draw(valueBidders));
         PrintWriter out = new PrintWriter(jsonOutputFileLocation);
         out.println(json_lsvm_draw);
         out.close();
